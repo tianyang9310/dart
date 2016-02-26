94ecab46c89d184104d9a590720c8ed3c0d0715d
YangTian <tianyang9310@gmail.com>
2016-02-26 11:51:28 -0500

    Paramenter Tuning TRY #2.1
    Former-commit-id: 62b6d05469e43536714697d9f588c0692c543661

--- tutorials/toy_example_MPC.cpp
+++ tutorials/toy_example_MPC.cpp
@@ -198,7 +198,7 @@ public:
 		//initialize the optimizer
 		AaltoGames::ControlPBP pbp;
 		const int nSamples = 20;	//N in the paper
-		int nTimeSteps     = 300;		//K in the paper, resulting in a 0.5s planning horizon
+		int nTimeSteps     = 100;		//K in the paper, resulting in a 0.5s planning horizon
 		//const float PI=3.1416f;	
 		const int nStateDimensions=6;
 		const int nControlDimensions=1;
