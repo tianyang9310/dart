c3aa64b5512050f76170e0b832dc10f89283c63a
yang-lab <tianyang9310@gmail.com>
2016-10-21 18:42:18 -0400

    Fix Lemke according to one algorithm found online

--- apps/Lemke_Fix/main.cpp
+++ apps/Lemke_Fix/main.cpp
@@ -1,21 +1,79 @@
 #include <iostream>
 
 #include "dart/dart.h"
-#include "dart/lcpsolver/Lemke.h"
+#include "MyLemke.h"
+// #include "dart/lcpsolver/Lemke.h"
 
 int main(int argc, char* argv[]) 
 {
     Eigen::MatrixXd testA;
-    testA.resize(1,1);
-    testA<< 1;
     Eigen::VectorXd testb;
+    Eigen::VectorXd* f;
+    int err;
+
+    f =  new Eigen::VectorXd(1);
+    testA.resize(1,1);
+    testA << 1;
     testb.resize(1);
     testb<< -1.5;
+    std::cout<<"Matrix A is"<<std::endl;
     std::cout<<testA<<std::endl;
+    std::cout<<"Vector b is"<<std::endl;
     std::cout<<testb<<std::endl;
-    Eigen::VectorXd* f =  new Eigen::VectorXd(1);
-    int err = dart::lcpsolver::Lemke(testA,testb,f);
-    std::cout<<err<<std::endl;
+    err = dart::lcpsolver::YT::Lemke(testA,testb,f);
+    std::cout<<"Lemke solution z is"<<std::endl;
     std::cout<<(*f)<<std::endl;
+    std::cout<<"Lemke error type is "<<err<<std::endl;
+
+    std::cout<<std::endl<<std::endl;
+    std::cout<<"-----Validation-----"<<std::endl;
+    std::cout<<"z: "<<std::endl;
+    std::cout<<(*f)<<std::endl;
+    std::cout<<"A*z+b: "<<std::endl;
+    std::cout<<testA*(*f) + testb<<std::endl;
+
+    std::cout<<"=========================================="<<std::endl;
+    f =  new Eigen::VectorXd(2);
+    testA.resize(2,2);
+    testA << 3.12, 0.1877, 0.1877, 3.254;
+    testb.resize(2);
+    testb<< -0.00662, -0.006711;
+    std::cout<<"Matrix A is"<<std::endl;
+    std::cout<<testA<<std::endl;
+    std::cout<<"Vector b is"<<std::endl;
+    std::cout<<testb<<std::endl;
+    err = dart::lcpsolver::YT::Lemke(testA,testb,f);
+    std::cout<<"Lemke solution z is"<<std::endl;
+    std::cout<<(*f)<<std::endl;
+    std::cout<<"Lemke error type is "<<err<<std::endl;
+
+    std::cout<<std::endl<<std::endl;
+    std::cout<<"-----Validation-----"<<std::endl;
+    std::cout<<"z: "<<std::endl;
+    std::cout<<(*f)<<std::endl;
+    std::cout<<"A*z+b: "<<std::endl;
+    std::cout<<testA*(*f) + testb<<std::endl;
+
+    std::cout<<"=========================================="<<std::endl;
+    f =  new Eigen::VectorXd(2);
+    testA.resize(2,2);
+    testA <<4.001 ,  -2 ,-2,3.999 ;
+    testb.resize(2);
+    testb<< -0.009819, -0.009798;
+    std::cout<<"Matrix A is"<<std::endl;
+    std::cout<<testA<<std::endl;
+    std::cout<<"Vector b is"<<std::endl;
+    std::cout<<testb<<std::endl;
+    err = dart::lcpsolver::YT::Lemke(testA,testb,f);
+    std::cout<<"Lemke solution z is"<<std::endl;
+    std::cout<<(*f)<<std::endl;
+    std::cout<<"Lemke error type is "<<err<<std::endl;
+
+    std::cout<<std::endl<<std::endl;
+    std::cout<<"-----Validation-----"<<std::endl;
+    std::cout<<"z: "<<std::endl;
+    std::cout<<(*f)<<std::endl;
+    std::cout<<"A*z+b: "<<std::endl;
+    std::cout<<testA*(*f) + testb<<std::endl;
 }
 
