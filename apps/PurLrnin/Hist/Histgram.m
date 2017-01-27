727b0b2e9c1811efab6f8bfed11c1e905ae86396
YangTian <tianyang9310@gmail.com>
2017-01-27 18:31:16 -0500

    change path of histgram

--- apps/PurLrnin/Hist/Histgram.m
+++ apps/PurLrnin/Hist/Histgram.m
@@ -1,7 +1,10 @@
 clc,clear;
 close all;
+
+path='/Users/Yang/Material/Research/dart/build/data/'
+
 figure(1);
-lcpdata4=csvread('/tmp/lcp_data4.csv');
+lcpdata4=csvread([path 'lcp_data4.csv']);
 subplot(2,2,1)
 hist(lcpdata4(:,end-3))
 title('contact 1')
@@ -19,7 +22,7 @@ title('contact 4')
 % clc,clear;
 % close all;
 figure(2);
-lcpdata3=csvread('/tmp/lcp_data3.csv');
+lcpdata3=csvread([path 'lcp_data3.csv']);
 subplot(2,2,1)
 hist(lcpdata3(:,end-2))
 title('contact 1')
@@ -31,15 +34,16 @@ hist(lcpdata3(:,end))
 title('contact 3')
 
 figure(3);
-lcpdata2=csvread('/tmp/lcp_data2.csv');
+lcpdata2=csvread([path 'lcp_data2.csv']);
 subplot(2,1,1)
 hist(lcpdata2(:,end-1))
 title('contact 1')
 subplot(2,1,2)
 hist(lcpdata2(:,end))
 title('contact 2')
+hist3(lcpdata2(:,end-1:end));
 
 figure(4);
-lcpdata1=csvread('/tmp/lcp_data1.csv');
+lcpdata1=csvread([path 'lcp_data1.csv']);
 hist(lcpdata1(:,end))
 title('contact 1')
